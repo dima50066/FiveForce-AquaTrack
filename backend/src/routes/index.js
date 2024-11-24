bcd78d2af1e6358ca2e61d4b6f067238c5e90af2
Yana Ushakova <115149484+yana1982@users.noreply.github.com>
2024-11-24 16:41:43 +0200

    update routes, services add ctrlWrapper
    controllers+routes+ctrlWrapper+services

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
+import {
+  createWaterController,
+  updateWaterController,
+  deleteWaterController,
+} from "../controllers/waters.js";
+import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 
 const router = Router();
+
+router.post("/api/water", ctrlWrapper(createWaterController));
+router.put("/api/water/:waterId", ctrlWrapper(updateWaterController));
+router.delete("/api/water/:waterId", ctrlWrapper(deleteWaterController));
 
 export default router;

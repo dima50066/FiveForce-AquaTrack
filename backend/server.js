c4030920b90534b57cd25b451a6ec1c77313b464
Kateryna-Zazuliak <kateryna_didenko@ukr.net>
2024-11-25 00:22:30 +0200

    add changes

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,11 @@ import cookieParser from 'cookie-parser';
 import pino from 'pino-http';
 import { errorHandler } from './src/middlewares/errorHandler.js';
 import { notFoundHandler } from './src/middlewares/notFoundHandler.js';
+import router from './src/routes/index.js';
 import corsOptions from './src/utils/corsConfig.js';
 import { fileURLToPath } from 'url';
 import path from 'path';
-import authRouter from './src/routes/auth.js';
+import swaggerDocs from './src/middlewares/swaggerDocs.js';
 
 dotenv.config();
 
@@ -19,7 +20,7 @@ const __dirname = path.dirname(__filename);
 
 const UPLOAD_DIR = path.join(__dirname, 'uploads');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 console.log(`Running in ${process.env.NODE_ENV || 'development'} mode`);
 
 export const setupServer = () => {
@@ -38,12 +39,14 @@ export const setupServer = () => {
   app.use(cors(corsOptions));
 
   app.use('/uploads', express.static(UPLOAD_DIR));
-  app.use('/auth/users', authRouter);
+  app.use(router);
 
   app.use((req, res, next) => {
     res.set('Cache-Control', 'no-store');
     next();
   });
+
+  app.use('/api-docs', swaggerDocs());
 
   app.use(notFoundHandler);
   app.use(errorHandler);
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import authRouter from './auth.js';
 
 const router = Router();
+
+router.use('/auth/users', authRouter);
 
 export default router;

0de54d228308925f40bb2118cfb44eaa35dfa54a
Dumitru <dima50066@gmail.com>
2024-12-04 21:36:49 +0100

    rename middlewares

--- backend/src/routes/water.js
+++ backend/src/routes/water.js
@@ -7,18 +7,18 @@ import {
   getMonthWater,
   getSummaryAmount,
 } from '../controllers/waters.js';
-import { checkToken } from '../middlewares/checkToken.js';
+import { auth } from '../middlewares/authenticate.js';
 import { WaterDate, WaterMonth } from '../middlewares/dateMiddleware.js';
 import { validateBody } from '../middlewares/validateBody.js';
 import { createWaterSchema, updateWaterSchema } from '../validation/water.js';
 
 const router = express.Router();
 
-router.post('/', checkToken, validateBody(createWaterSchema), createWater);
-router.delete('/:id', checkToken, deleteWater);
-router.put('/:id', checkToken, updateWater, validateBody(updateWaterSchema));
-router.get('/', checkToken, getSummaryAmount);
-router.get('/day/:date', WaterDate, checkToken, getDayWater);
-router.get('/month/:date', WaterMonth, checkToken, getMonthWater);
+router.post('/', auth, validateBody(createWaterSchema), createWater);
+router.delete('/:id', auth, deleteWater);
+router.put('/:id', auth, updateWater, validateBody(updateWaterSchema));
+router.get('/', auth, getSummaryAmount);
+router.get('/day/:date', WaterDate, auth, getDayWater);
+router.get('/month/:date', WaterMonth, auth, getMonthWater);
 
 export default router;
